b37a8f6c80b25452442013ac15df5c12f11d5cc9
廖延 <363918226@qq.com>
2017-07-12 18:17:52 +0800

    set 22 before send email

--- gold/spiders/goldprice.py
+++ gold/spiders/goldprice.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import scrapy
 from selenium import webdriver
+import datetime
 
 class GoldpriceSpider(scrapy.Spider):
     name = 'goldprice'
@@ -18,24 +19,26 @@ class GoldpriceSpider(scrapy.Spider):
 
 
     def close(self,spider, reason):
+        hour = datetime.datetime.now().hour
         if(self.price != None):
-            if(self.price > 278 or self.price < 270):
-                from scrapy.mail import MailSender
-                # mailer = MailSender.from_settings(settings)# 出错了，没找到原因
-                mailer = MailSender(
-                    smtphost = "smtp.163.com",  # 发送邮件的服务器
-                    mailfrom = "18607970065@163.com",   # 邮件发送者
-                    smtpuser = "18607970065@163.com",   # 用户名
-                    smtppass = "yan18779865344",  # 发送邮箱的密码不是你注册时的密码，而是授权码！！！切记！
-                    smtpport = 25   # 端口号
-                )
+            if int(hour) < 22:
+                if(self.price > 278 or self.price < 270):
+                    from scrapy.mail import MailSender
+                    # mailer = MailSender.from_settings(settings)# 出错了，没找到原因
+                    mailer = MailSender(
+                        smtphost = "smtp.163.com",  # 发送邮件的服务器
+                        mailfrom = "18607970065@163.com",   # 邮件发送者
+                        smtpuser = "18607970065@163.com",   # 用户名
+                        smtppass = "yan18779865344",  # 发送邮箱的密码不是你注册时的密码，而是授权码！！！切记！
+                        smtpport = 25   # 端口号
+                    )
 
-                body = u"""
-                实时爬取的黄金价格为:
-                """ + str(self.price)
-                subject = u'爬取的黄金实时价格'
-                # 如果说发送的内容太过简单的话，很可能会被当做垃圾邮件给禁止发送。
-                mailer.send(to=["363918226@qq.com"], subject = subject.encode("utf-8"), body = body.encode("utf-8"))
+                    body = u"""
+                    实时爬取的黄金价格为:
+                    """ + str(self.price)
+                    subject = u'爬取的黄金实时价格'
+                    # 如果说发送的内容太过简单的话，很可能会被当做垃圾邮件给禁止发送。
+                    mailer.send(to=["363918226@qq.com"], subject = subject.encode("utf-8"), body = body.encode("utf-8"))
 
     def __del__(self):
         self.browser.close()
